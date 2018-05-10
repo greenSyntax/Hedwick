079129ce496b3cbb4b30c6632b9cb6dfb3138012
Abhishek  Kumar Ravi <abhishek.ravi@thoughts2binary.com>
2018-05-10 14:12:50 +0530

    Change in App

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit22f17c55b3191e5a1d7d8cbeba8dc2a7
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit22f17c55b3191e5a1d7d8cbeba8dc2a7', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
